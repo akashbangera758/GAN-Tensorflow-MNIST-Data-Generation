9fc05a0cd3f3a4abe9ccabc2fbe1c566af58d4ab
Akash Bangera <32443012+akashbangera758@users.noreply.github.com>
2018-06-23 09:22:42 +0530

    Uploaded gan_mnist.py

---
+++ gan_mnist.py
@@ -0,0 +1,112 @@
+import pickle
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data")
+
+
+def model_inputs(real_dim, z_dim):
+    inputs_real = tf.placeholder(dtype=tf.float32, shape=(None, real_dim), name="inputs_real")
+    inputs_z = tf.placeholder(dtype=tf.float32, shape=(None, z_dim), name="inputs_z")
+
+    return inputs_real, inputs_z
+
+
+def generator(z, out_dim, n_units=128, reuse=False, alpha=0.01):
+    with tf.variable_scope(name_or_scope="generator", reuse=reuse):
+        h1 = tf.layers.dense(inputs=z, units=n_units, activation=None)
+        h1 = tf.maximum(alpha*h1, h1)
+        logits = tf.layers.dense(inputs=h1, units=out_dim, activation=None)
+        out = tf.tanh(logits)
+
+        return out
+
+
+def discriminator(x, n_units=128, reuse=False, alpha=0.01):
+    with tf.variable_scope(name_or_scope="discriminator", reuse=reuse):
+        h1 = tf.layers.dense(inputs=x, units=n_units, activation=None)
+        h1 = tf.maximum(alpha*h1, h1)
+        logits = tf.layers.dense(inputs=h1, units=1, activation=None)
+        out = tf.sigmoid(logits)
+
+        return out, logits
+
+input_size = 784
+z_size = 100
+g_hidden_size = 128
+d_hidden_size = 128
+alpha = 0.01
+smooth = 0.1
+
+tf.reset_default_graph()
+
+input_real, input_z = model_inputs(input_size, z_size)
+
+g_model = generator(input_z, input_size, n_units=g_hidden_size, alpha=alpha)
+
+d_model_real, d_logits_real = discriminator(input_real, n_units=d_hidden_size, alpha=alpha)
+d_model_fake, d_logits_fake = discriminator(g_model, n_units=d_hidden_size, reuse=True, alpha=alpha)
+
+d_loss_real = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=d_logits_real, labels=tf.ones_like(d_logits_real)*(1-smooth)))
+d_loss_fake = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=d_logits_fake, labels=tf.zeros_like(d_logits_real)))
+
+d_loss = d_loss_real + d_loss_fake
+
+g_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=d_logits_fake, labels=tf.ones_like(d_logits_fake)))
+
+learning_rate = 0.002
+
+t_vars = tf.trainable_variables()
+g_vars = [var for var in t_vars if var.name.startswith("generator")]
+d_vars = [var for var in t_vars if var.name.startswith("discriminator")]
+
+g_train_opt = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss=g_loss, var_list=g_vars)
+d_train_opt = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss=d_loss, var_list=d_vars)
+
+
+# Training
+batch_size = 100
+epochs = 100
+samples = []
+losses = []
+saver = tf.train.Saver(var_list=g_vars)
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for e in range(epochs):
+        for ii in range(mnist.train.num_examples//batch_size):
+            batch = mnist.train.next_batch(batch_size)
+
+            batch_images = batch[0].reshape((batch_size, 784))
+            batch_images = batch_images*2 - 1
+
+            batch_z = np.random.uniform(-1, 1, size=(batch_size, z_size))
+            _ = sess.run(d_train_opt, feed_dict={input_real: batch_images, input_z: batch_z})
+            _ = sess.run(g_train_opt, feed_dict={input_z: batch_z})
+
+        train_loss_d = sess.run(d_loss, feed_dict={input_z: batch_z, input_real: batch_images})
+        train_loss_g = g_loss.eval({input_z: batch_z})
+
+        print("Epoch {}/{}...".format(e + 1, epochs),
+              "Discriminator Loss: {:.4f}...".format(train_loss_d),
+              "Generator Loss: {:.4f}".format(train_loss_g))
+        losses.append((train_loss_d, train_loss_g))
+
+        sample_z = np.random.uniform(-1, 1, size=(16, z_size))
+        gen_samples = sess.run(
+            generator(input_z, input_size, n_units=g_hidden_size, reuse=True, alpha=alpha),
+            feed_dict={input_z: sample_z})
+        samples.append(gen_samples)
+        saver.save(sess, './checkpoints/generator.ckpt')
+
+with open('train_samples.pkl', 'wb') as f:
+    pickle.dump(samples, f)
+
+fig, ax = plt.subplots()
+losses = np.array(losses)
+plt.plot(losses.T[0], label='Discriminator')
+plt.plot(losses.T[1], label='Generator')
+plt.title("Training Losses")
+plt.legend()
+
